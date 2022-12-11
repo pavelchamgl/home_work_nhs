d3dba0eb84f3a73712f264d1c22685dbcd01224b
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2022-12-11 10:41:10 +0300

    add homework_12

--- task_12/task_12.py
+++ task_12/task_12.py
@@ -21,7 +21,7 @@ COUNTRIES = ["Belarus", "Poland", "Australia", "Russia", "Germany", "USA"]
 
 
 class User:
-    def __init__(self, name, surname, age, country, gender, profession):
+    def __init__(self, name, surname, age, country, gender, profession=None):
         self.name = name
         self.surname = surname
         self.age = age
@@ -35,31 +35,31 @@ class User:
     def birth_year(self):
         return int(datetime.now().year - self.age)
 
-    def repr(self):
+    def __repr__(self):
         return f"{self.name} {self.surname}\nE-mail - <{self.email()}>\nBirth-year - {self.birth_year()}\n" \
                f"Age - {self.age}\nCountry - {self.country}\nGender - {self.gender}\n" \
                f"Profession - {self.profession}"
 
 
-def generate_user(profession):
-    gender_1 = randint(0, 1)
-    if gender_1 == 0:
-        return User(name=NAME_MEN[randint(0, len(NAME_MEN) - 1)], age=randint(20, 100),
-                    country=COUNTRIES[randint(0, len(COUNTRIES) - 1)],
-                    gender="Man", profession=profession, surname=SURNAMES[randint(0, len(SURNAMES) - 1)])
-    else:
-        return User(name=NAME_WOMEN[randint(0, len(NAME_WOMEN) - 1)], age=randint(20, 100),
-                    country=COUNTRIES[randint(0, len(COUNTRIES) - 1)],
-                    gender="Woman", profession=profession, surname=SURNAMES[randint(0, len(SURNAMES) - 1)])
+    def generate_user(profession):
+        gender = randint(0, 1)
+        if gender == 0:
+            return User(name=NAME_MEN[randint(0, len(NAME_MEN) - 1)], age=randint(20, 100),
+                        country=COUNTRIES[randint(0, len(COUNTRIES) - 1)],
+                        gender="Man", profession=profession, surname=SURNAMES[randint(0, len(SURNAMES) - 1)])
+        else:
+            return User(name=NAME_WOMEN[randint(0, len(NAME_WOMEN) - 1)], age=randint(20, 100),
+                        country=COUNTRIES[randint(0, len(COUNTRIES) - 1)],
+                        gender="Woman", profession=profession, surname=SURNAMES[randint(0, len(SURNAMES) - 1)])
 
 
-def prof_teacher():
-    return generate_user("Teacher")
+    def prof_teacher():
+        return User.generate_user(profession="Teacher")
 
 
-def prof_doctor():
-    return generate_user("Doctor")
+    def prof_doctor():
+        return User.generate_user(profession="Doctor")
 
 
-def prof_policeman():
-    return generate_user("Policeman")
+    def prof_policeman():
+        return User.generate_user(profession="Policeman")
